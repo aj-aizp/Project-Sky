83dcfd44d400c7427a1d4a76774161c32379854b
AJ Aizpurua <142620573+aj-aizp@users.noreply.github.com>
2024-08-16 12:03:10 -0400

    Commit 2
    Player shooting mechanic worked out

---
+++ Project Sky/Assets/Scripts/Player/Gun.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+   [SerializeField] private GameObject bulletPrefab; 
+   [SerializeField] private float projectileSpeed; 
+   [SerializeField] private float fireRate; 
+
+   private Boolean canShoot = true; 
+
+
+    private void Update() {
+        if (Input.GetMouseButton(0) && canShoot ==true) {
+
+            //Use Coroutine because it will enable me to control timing of shots
+
+            StartCoroutine(Shoot());  
+
+        }
+
+
+        IEnumerator Shoot() {
+
+            canShoot = false; 
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity); 
+
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>(); 
+
+            rb.velocity = transform.right * projectileSpeed; 
+            yield return new WaitForSeconds(fireRate);
+
+            canShoot = true; 
+
+            Destroy(bullet,1f); 
+        }
+    }
+
+
+
+}
