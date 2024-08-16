bbcc80a220c505c48e2725c241c2e7e684514ded
AJ Aizpurua <142620573+aj-aizp@users.noreply.github.com>
2024-08-16 13:11:51 -0400

    Commit 3
    Enemy Projectiles

---
+++ Project Sky/Assets/Scripts/Enemies/GunEnemy.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunEnemy : MonoBehaviour
+{
+   [SerializeField] private GameObject bulletPrefab; 
+   [SerializeField] private float projectileSpeed; 
+   [SerializeField] private float fireRate; 
+
+   private Boolean canShoot = true; 
+
+
+    private void Update() {
+        if (canShoot ==true) {
+
+            //Use Coroutine because it will enable me to control timing of shots
+
+            StartCoroutine(Shoot());  
+
+        }
+
+
+        IEnumerator Shoot() {
+
+            canShoot = false; 
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity); 
+
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>(); 
+
+            rb.velocity = (-transform.right) * projectileSpeed; 
+
+            yield return new WaitForSeconds(fireRate);
+
+            canShoot = true; 
+
+            Destroy(bullet,5f); 
+        }
+    }
+
+}
